da66ee23e3aa5651d8237f94dd529f51e5de7be0
Gabriel9706374 <gabriel9706374@rede.ulbra.br>
2026-09-22 22:51:20 -0300

    Telas em java

---
+++ Sistema_Escolar/src/main/java/com/mycompany/sistema_escolar/Sistema_Escolar.java
@@ -0,0 +1,10 @@
+package com.mycompany.sistema_escolar;
+
+import view.Menu;
+public class Sistema_Escolar {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setVisible(true);
+    }
+}
